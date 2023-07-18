cc1c0eb3ed23d4a8ea6aaadaaeb157ffb7c8453d
Artem <fedorzvyk@gmail.com>
2023-07-18 20:19:50 +0300

    new

--- index.js
+++ index.js
@@ -1,19 +1,131 @@
-const numberOfFilms = +prompt('how many films did you watch?');
+"use strict"; 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}
 
-const filmName = prompt('Один из последних просмотренных фильмов?');
-const filmRate = prompt('На сколько оцените его?');
-const filmName2 = prompt('Один из последних просмотренных фильмов?');
-const filmRate2 = prompt('На сколько оцените его?');
+// const numberOfFilms = +prompt('how many films did you watch?');
 
-personalMovieDB.movies[filmName] = filmRate;
-personalMovieDB.movies[filmName2] = filmRate2;
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
 
-console.log(personalMovieDB)
+// const filmName = prompt('Один из последних просмотренных фильмов?');
+// const filmRate = prompt('На сколько оцените его?');
+// const filmName2 = prompt('Один из последних просмотренных фильмов?');
+// const filmRate2 = prompt('На сколько оцените его?');
+
+// personalMovieDB.movies[filmName] = filmRate;
+// personalMovieDB.movies[filmName2] = filmRate2;
+
+// console.log(personalMovieDB)
+
+
+// const a = 0;
+
+// if (a) {
+// console.log('true')
+// } else console.log('false')
+
+// let a, b=1, c;
+
+// console.log(a || b || c)
+
+
+// for (let i = 0; i < 3; i++){
+//     console.log(i);
+//     for (let j = 0; j < 3; j++){
+//     console.log(j);
+// }
+// }
+
+// let result = '';
+
+// const length = 7;
+
+// for (let i = 1; i <= length; i++){
+
+//     for (let j = 0; j < i; j++){
+//   result += '*'
+//     }
+//     result += '\n'
+// }
+
+
+// console.log(result)
+
+
+
+////////////////////////////////////////////////////
+
+// first: for (let i = 0; i < 3; i++){
+//     console.log(`first level: ${i}`);
+//     for (let j = 0; j < 3; j++){
+//         console.log(`second level: ${j}`);
+//         for (let k = 0; k < 3; k++){
+//             if (k === 2) break first;
+//     console.log(`third level: ${k}`);
+// }
+// }
+// }
+
+///////////////////////////////////////
+
+// for (let i = 2; i <= 16; i++) {
+//      if (i % 2 === 0) {
+//          continue;
+//      } else {
+//          console.log(i);
+//      }
+//  }
+
+//  const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//     // Пишем решение вот тут
+//     for (let i=0; i<data.length;i++){
+//         if ( typeof(data[i]) === "number" ){
+//             data[i] *=2;
+//         } else  data[i]+=' - done'
+//     }
+//     // Не трогаем
+//     console.log(data) ;
+
+
+/////////////////////////////////////
+    
+
+// const lines = 5;
+// let result = '';
+
+
+// for (let i = 1; i <= lines; i++){
+   
+//         for (let j = 0; j < i; j++) {
+       
+//             result += '*'
+//     }
+    
+//         result += '\n';
+    
+// }
+
+
+// console.log(result)
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+    
